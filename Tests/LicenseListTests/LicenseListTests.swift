e060e3183841115d84951f1c8d46124785d898ab
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-07-06 15:40:45 +0900

    Fixed LicenseListTests

--- Tests/LicenseListTests/LicenseListTests.swift
+++ Tests/LicenseListTests/LicenseListTests.swift
@@ -2,15 +2,25 @@ import XCTest
 @testable import LicenseList
 
 final class LicenseListTests: XCTestCase {
-
     func testParsePlist() throws {
         let url = Bundle.module.url(forResource: "license-list", withExtension: "plist")!
         let sut = LicenseListView(fileURL: url)
 
-        XCTAssertEqual(sut.libraries.count, 2)
-        XCTAssertEqual(sut.libraries[0].name, "KeychainAccess")
-        XCTAssertEqual(sut.libraries[0].licenseType, "MIT License")
-        XCTAssertEqual(sut.libraries[1].name, "swift-argument-parser")
+        XCTAssertEqual(sut.libraries.count, 5)
+
+        XCTAssertEqual(sut.libraries[0].name, "Package-A")
+        XCTAssertEqual(sut.libraries[0].licenseType, "Unknown License")
+
+        XCTAssertEqual(sut.libraries[1].name, "Package-B")
         XCTAssertEqual(sut.libraries[1].licenseType, "Apache license 2.0")
+
+        XCTAssertEqual(sut.libraries[2].name, "Package-C")
+        XCTAssertEqual(sut.libraries[2].licenseType, "MIT License")
+
+        XCTAssertEqual(sut.libraries[3].name, "Package-D")
+        XCTAssertEqual(sut.libraries[3].licenseType, "BSD 3-clause Clear license")
+
+        XCTAssertEqual(sut.libraries[4].name, "Package-E")
+        XCTAssertEqual(sut.libraries[4].licenseType, "zLib License")
     }
 }
