7d9badf8bdfe299cb33e4fea7a318fbaf32acee0
SNQ-2001 <taishin05109@gmail.com>
2023-04-05 15:27:36 +0900

    feat: Libraryを配列で取得できるようにlicensesメソッドを追加

--- Sources/LicenseList/Library.swift
+++ Sources/LicenseList/Library.swift
@@ -12,3 +12,27 @@ public struct Library: Hashable {
     public let licenseType: String
     public let licenseBody: String
 }
+
+extension Library {
+    static public var licenses: [Library] {
+        var libraries: [Library] = []
+        guard let fileURL = Bundle.main.url(forResource: "license-list", withExtension: "plist"),
+              let data = try? Data(contentsOf: fileURL),
+              let plist = try? PropertyListSerialization.propertyList(from: data, format: nil),
+              let dict = plist as? [String: Any] else {
+            return libraries
+        }
+        let _libraries = (dict["libraries"] as? [[String: Any]])?.compactMap({ info -> Library? in
+            guard let name = info["name"] as? String,
+                  let type = info["licenseType"] as? String,
+                  let body = info["licenseBody"] as? String else {
+                return nil
+            }
+            return Library(name: name,
+                           licenseType: type,
+                           licenseBody: body)
+        })
+        guard let libraries = _libraries else { return libraries }
+        return libraries
+    }
+}
