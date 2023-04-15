74086e95aa016d3ad4fb39219bd5d9332432feb1
SNQ-2001 <info@taishin-miyamoto.com>
2023-04-15 22:29:08 +0900

    fix: Library.libraries()を修正

--- Sources/LicenseList/Library.swift
+++ Sources/LicenseList/Library.swift
@@ -14,24 +14,15 @@ public struct Library: Hashable {
 
 extension Library {
     static public var libraries: [Library] {
-        var libraries: [Library] = []
         guard let fileURL = Bundle.main.url(forResource: "license-list", withExtension: "plist"),
               let data = try? Data(contentsOf: fileURL),
               let plist = try? PropertyListSerialization.propertyList(from: data, format: nil),
               let dict = plist as? [String: Any] else {
-            return libraries
+            return []
         }
-        let _libraries = (dict["libraries"] as? [[String: Any]])?.compactMap({ info -> Library? in
-            guard let name = info["name"] as? String,
-                  let type = info["licenseType"] as? String,
-                  let body = info["licenseBody"] as? String else {
-                return nil
-            }
-            return Library(name: name,
-                           licenseType: type,
-                           licenseBody: body)
-        })
-        guard let libraries = _libraries else { return libraries }
-        return libraries
+        return (dict["libraries"] as? [[String: Any]])?.compactMap({ info -> Library? in
+            guard let name = info["name"] as? String, let type = info["licenseType"] as? String else { return nil }
+            return Library(name: name, licenseBody: body)
+        }) ?? []
     }
 }
