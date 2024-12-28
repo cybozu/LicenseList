0aa4687121c063dcba0a0ecbcb0359f1c2125a3c
Takuto NAKAMURA (Kyome) <takuto-nakamura@cybozu.co.jp>
2024-12-28 22:32:59 +0900

    Testing Build on CI

---
+++ Examples/ExamplesForSwiftUITests/ExamplesForSwiftUITests.swift
@@ -0,0 +1,13 @@
+//
+//  ExamplesForSwiftUITests.swift
+//  ExamplesForSwiftUITests
+//
+//  Created by ky0me22 on 2024/12/28.
+//
+
+import XCTest
+import ExamplesForSwiftUI
+
+final class ExamplesForSwiftUITests: XCTestCase {
+    func testExample() throws {}
+}
